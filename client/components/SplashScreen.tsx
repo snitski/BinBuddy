ee6f4f18964f503a95833c1a4a770a1eac703640
Xaladow <105936887+Xaladow@users.noreply.github.com>
2023-03-04 17:10:41 -0500

    Fixed typescript error

--- client/components/SplashScreen.tsx
+++ client/components/SplashScreen.tsx
@@ -3,38 +3,41 @@ import { Image } from 'expo-image';
 import { useAssets } from 'expo-asset';
 import { useFonts, Lato_400Regular, Lato_900Black } from '@expo-google-fonts/lato';
 
-export default function SplashScreen(props: {
-    setCameraIsOpen: (cameraIsOpen: boolean) => void
-}) {
+export default function SplashScreen(props: { setCameraIsOpen: (cameraIsOpen: boolean) => void }) {
     const [assets, error] = useAssets([require('../assets/logo.png')]);
     const [fontsLoaded] = useFonts({
         Lato_400Regular,
-        Lato_900Black
+        Lato_900Black,
     });
 
-    if(!fontsLoaded)
-        return null;
+    if (!fontsLoaded) return null;
     return (
-        <TouchableOpacity 
-            style={{flex: 1, width: '100%', backgroundColor: '#609966'}} 
-            onPress={() => props.setCameraIsOpen(true)}
-        >
+        <TouchableOpacity
+            style={{ flex: 1, width: '100%', backgroundColor: '#609966' }}
+            onPress={() => props.setCameraIsOpen(true)}>
             <View style={styles.top}>
-                {assets ?
-                    <Image 
-                        style={{width: 215, height: 215}} 
-                        source={assets[0]} 
-                        contentFit='contain'
+                {assets ? (
+                    <Image
+                        style={{ width: 215, height: 215 }}
+                        source={assets[0] as any}
+                        contentFit="contain"
                     />
-                :
+                ) : (
                     <Text>Failed to load logo.</Text>
-                }
-                    <Text style={{fontFamily: 'Lato_900Black', color:'#EDF1D6', fontSize: 26}}>
-                        Welcome to BinBuddy!
-                    </Text>
-                    <Text style={{fontFamily: 'Lato_900Black', color:'#EDF1D6', fontSize: 18, alignSelf: 'center', margin: 2}}>
-                        Your AI recycling companion
-                    </Text>
+                )}
+                <Text style={{ fontFamily: 'Lato_900Black', color: '#EDF1D6', fontSize: 26 }}>
+                    Welcome to BinBuddy!
+                </Text>
+                <Text
+                    style={{
+                        fontFamily: 'Lato_900Black',
+                        color: '#EDF1D6',
+                        fontSize: 18,
+                        alignSelf: 'center',
+                        margin: 2,
+                    }}>
+                    Your AI recycling companion
+                </Text>
             </View>
 
             {/* <View style={styles.middle}>
@@ -52,7 +55,15 @@ export default function SplashScreen(props: {
             </View> */}
 
             <View style={styles.bottom}>
-                <Text style={{fontFamily: 'Lato_900Black', color:'#EDF1D6', fontSize: 16, margin: 15}}>Tap anywhere to scan</Text>
+                <Text
+                    style={{
+                        fontFamily: 'Lato_900Black',
+                        color: '#EDF1D6',
+                        fontSize: 16,
+                        margin: 15,
+                    }}>
+                    Tap anywhere to scan
+                </Text>
             </View>
         </TouchableOpacity>
     );
@@ -64,7 +75,7 @@ const styles = StyleSheet.create({
         width: '100%',
         alignItems: 'center',
         justifyContent: 'center',
-        transform: 'translateY(-50px)'
+        transform: 'translateY(-50px)' as any,
     },
     // middle: {
     //     flex: 5,
@@ -76,12 +87,12 @@ const styles = StyleSheet.create({
     bottom: {
         position: 'absolute',
         bottom: 0,
-        alignSelf: 'center'
+        alignSelf: 'center',
     },
     // middleText: {
-    //     fontFamily: 'Lato_900Black', 
-    //     color:'#EDF1D6', 
-    //     fontSize: 14, 
+    //     fontFamily: 'Lato_900Black',
+    //     color:'#EDF1D6',
+    //     fontSize: 14,
     //     paddingLeft: 20,
     //     paddingRight: 20,
     //     width: '100%',
